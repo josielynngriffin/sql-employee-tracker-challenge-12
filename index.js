c2888a2a5b958fca2d7e5d0bdf3bc22e2bc83ad6
josielynngriffin <jelynngriffin@gmail.com>
2023-06-25 19:46:53 -0400

    Add additional prompts for inquirer

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the employees database')
 );
+//prompt variables
 const questions = ([
     {
         type: 'list',
@@ -24,12 +25,78 @@ const questions = ([
     }
 ])
 
+const addDepartment =[{
+    type: 'input',
+    name: 'departmentName',
+    message: 'Enter new department name:'
+}];
+
+const addRole =[{
+    type:'input',
+    name:'title',
+    message: 'Enter title of new role:'
+},
+{
+    type: 'input',
+    name:'salary',
+    message: "Enter value of the new role's yearly salary:"
+},
+{
+    type: 'input',
+    name: 'department_id',
+    message: 'Enter corresponding department id for role:'
+}];
+
+const addEmployee = [{
+    type:'input',
+    name: 'firstName',
+    message: "Enter new employee's first name:"
+},
+{
+    type:'input',
+    name: 'lastName',
+    message: "Enter new employee's last name:"
+},
+{
+    type:'input',
+    name:'role_id',
+    message: "Enter new employee's role id:"
+},
+{
+    type:'input',
+    name:'manager_id',
+    message: "Enter the id of new employee's manager:"
+}];
+
+const updateRole = [{
+    type:'input',
+    name: 'firstName',
+    message: "Whose role do you wish to update? Enter the employee's first name"
+}, 
+{
+    type:'input',
+    name:'role_id',
+    message:"Enter the employee's new role id:"
+}];
 function init() {
     //inquirer prompt
     inquirer.prompt(questions)
     .then(data => {
         switch(data.startPrompt) {
             case 'View all departments':
+                break;
+            case 'View all roles':
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'Add a department':
+                break;
+            case 'Add a role':
+                break;
+            case 'Add an employee':
+                break;
+            case "Update an employee's role":
                 break;
         }
     })
